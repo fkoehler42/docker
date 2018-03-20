bab007f41e8714229b29ac0991ad40e8d8655052
Flavien <fkoehler@student.42.fr>
2018-03-20 20:27:04 +0100

    update ex03 and my_linux_env (WIP on both)

--- 02_bonus/my_linux_env/build_and_run.sh
+++ 02_bonus/my_linux_env/build_and_run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-docker build -t my_linux_env ~/projects/docker/02_bonus/my_linux_env
+
+BASEDIR=$(dirname "$0")"/"
+
+cp -r ~/.ssh $BASEDIR
+mv $BASEDIR".ssh/known_hosts" $BASEDIR"tmp"
+cat $BASEDIR"tmp" | grep "github" > $BASEDIR".ssh/known_hosts"
+rm $BASEDIR"tmp"
+docker build -t my_linux_env $BASEDIR
 docker run -it --name my_linux my_linux_env
