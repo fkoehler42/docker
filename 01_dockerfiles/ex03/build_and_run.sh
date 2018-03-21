882477e08ebbeea0fdeadbf7dd2f35a01c643ced
Flavien <fkoehler@student.42.fr>
2018-03-21 20:56:47 +0100

    update ex03 (almost done...I hope^^)

--- 01_dockerfiles/ex03/build_and_run.sh
+++ 01_dockerfiles/ex03/build_and_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build -t gitlab-ce ~/projects/docker/01_dockerfiles/ex03
 # port 22 already used (default ssh) so we set 2222
-docker run -it --name ex03 -p 443:443 -p 80:80 -p 2222:22 gitlab-ce
+docker run -it --name ex03 -p 80:80 -p 2222:22 gitlab-ce
