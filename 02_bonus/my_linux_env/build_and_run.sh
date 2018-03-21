d051c3742cd277836b04e37dce32335af093d575
Flavien <fkoehler@student.42.fr>
2018-03-21 12:36:54 +0100

    update my_linux_env

--- 02_bonus/my_linux_env/build_and_run.sh
+++ 02_bonus/my_linux_env/build_and_run.sh
@@ -6,5 +6,6 @@ cp -r ~/.ssh $BASEDIR
 mv $BASEDIR".ssh/known_hosts" $BASEDIR"tmp"
 cat $BASEDIR"tmp" | grep "github" > $BASEDIR".ssh/known_hosts"
 rm $BASEDIR"tmp"
-docker build -t my_linux_env $BASEDIR
+docker build -t my_linux_env --build-arg extra_repo=ft_ls $BASEDIR
+rm -rf $BASEDIR".ssh"
 docker run -it --name my_linux my_linux_env
