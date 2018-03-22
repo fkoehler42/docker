326651ff15fe3ed4db8f4b021dddf20d1320f3d2
Flavien <fkoehler@student.42.fr>
2018-03-22 20:45:00 +0100

    ex03 finally works (HTTP + SSH) \o/

--- 01_dockerfiles/ex03/build_and_run.sh
+++ 01_dockerfiles/ex03/build_and_run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 docker build -t gitlab-ce ~/projects/docker/01_dockerfiles/ex03
-# port 22 already used (default ssh) so we set 2222
-docker run -it --name ex03 -p 80:80 -p 2222:22 gitlab-ce
+docker run -it --name ex03 -p 80:80 -p 30022:22 gitlab-ce
