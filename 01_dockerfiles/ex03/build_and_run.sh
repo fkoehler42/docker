582137124eb6de855e025aeef0b85ff59148de8d
Flavien <fkoehler@student.42.fr>
2018-03-23 14:35:07 +0100

    update ex03 (gitlab server) - its all good

--- 01_dockerfiles/ex03/build_and_run.sh
+++ 01_dockerfiles/ex03/build_and_run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-docker build -t gitlab-ce ~/projects/docker/01_dockerfiles/ex03
-docker run -it --name ex03 -p 80:80 -p 30022:22 gitlab-ce
+
+BASEDIR=$(dirname "$0")"/"
+
+docker build -t gitlab-ce $BASEDIR
+docker run -d --name ex03 -p 80:80 -p 30022:22 gitlab-ce
